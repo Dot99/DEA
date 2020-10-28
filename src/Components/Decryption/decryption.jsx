846ce83a7e0db2f5a709f85cbfe5d44df82c8ac8
CDGuedes <carlos.guedes.12.98@gmail.com>
2020-10-28 17:36:04 +0000

    Added some boilerplate and placeholder values to the components that'll represent pages

---
+++ src/Components/Decryption/decryption.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Decryption(props) {
+  return <div>Decr</div>;
+}
+
+export default Decryption;
---
+++ src/Components/Encryption/encryption.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Encryption(props) {
+  return <div>Encr</div>;
+}
+
+export default Encryption;
