d69eedafd1c94ea9c931e16f858ccf7d96368bb2
CDGuedes <carlos.guedes.12.98@gmail.com>
2020-10-28 17:35:29 +0000

    Added CSSTransition and router to main component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,48 @@
 import React from "react";
+import { CSSTransition } from "react-transition-group";
+import { Route } from "react-router-dom";
+
+import Encryption from "./Components/Encryption/encryption";
+import Decryption from "./Components/Decryption/decryption";
+import Landing from "./Components/Landing/landing";
 
 function App() {
-  return <div className="App">App</div>;
+  const PAGES = [
+    {
+      path: "/",
+      component: <Landing />,
+      exact: true,
+    },
+    {
+      path: "/encryption",
+      component: <Encryption />,
+      exact: false,
+    },
+    {
+      path: "/decryption",
+      component: <Decryption />,
+      exact: false,
+    },
+  ];
+
+  return (
+    <React.Fragment>
+      {PAGES.map(({ path, component, exact }) => (
+        <Route key={path} path={path} exact={exact}>
+          {({ match }) => (
+            <CSSTransition
+              in={match != null}
+              timeout={3000}
+              unmountOnExit
+              appear
+            >
+              {component}
+            </CSSTransition>
+          )}
+        </Route>
+      ))}
+    </React.Fragment>
+  );
 }
 
 export default App;
