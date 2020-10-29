bbb330222037eda51a5eba32e642fc8b93c262e3
CDGuedes <carlos.guedes.12.98@gmail.com>
2020-10-29 20:17:28 +0000

    Added both buttons to the landing page

--- src/Components/Landing/landing.jsx
+++ src/Components/Landing/landing.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import styled from "styled-components";
 
-function Landing(props) {
-  return <div>landing</div>;
+import { ReactComponent as Decrypt } from "../../Assets/Decrypt.svg";
+import { ReactComponent as Encrypt } from "../../Assets/Encrypt.svg";
+
+import SvgHalfPageButton from "../Basic/svgHalfPageButton";
+
+const Container = styled.main`
+  background-color: #2c2c2c;
+`;
+
+function Landing({ shouldRender }) {
+  return (
+    <Container>
+      <SvgHalfPageButton shouldRender={shouldRender}>
+        <Decrypt />
+      </SvgHalfPageButton>
+      <SvgHalfPageButton shouldRender={shouldRender}>
+        <Encrypt />
+      </SvgHalfPageButton>
+    </Container>
+  );
 }
 
 export default Landing;
