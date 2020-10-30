802f4138b919b884a642eded15b698955bba33b4
CDGuedes <carlos.guedes.12.98@gmail.com>
2020-10-30 12:52:33 +0000

    Added animation to the enter and exit page

--- src/Components/Landing/landing.jsx
+++ src/Components/Landing/landing.jsx
@@ -9,6 +9,27 @@ import SvgHalfPageButton from "../Basic/svgHalfPageButton";
 
 const Container = styled.main`
   background-color: #2c2c2c;
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  z-index: 99;
+
+  transition: transform var(--page-transition-duration) ease-in-out;
+
+  &.enter {
+    transform: translateY(100vh);
+  }
+  &.enter-active {
+    transform: translateY(0);
+  }
+  &.exit {
+    transform: translateY(0);
+  }
+  &.exit-active {
+    transform: translateY(100vh);
+  }
 `;
 
 function Landing({ shouldRender }) {
